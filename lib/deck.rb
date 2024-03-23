f97057886bb6b7b5784b003135c33932f99506e1
Md Farukuzzaman <farukuzzaman.bd@gmail.com>
2024-03-23 18:56:15 -0500

    added logic and test cases for deal class that shuffles and deals cards to the players.

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,28 @@ class Deck
     return @cards
   end
 
+  def shuffle_cards
+    @cards.shuffle!()
+    return @cards
+  end
+
+  def deal_cards(shuffled_cards)
+    result = []
+    players = ['1', '2', '3', '4']
+    # puts shuffled_cards
+    i = -1
+    players.each do |player|
+      cards = []
+      i = i + 1
+      card_index = i
+      (1...6).each do |n|
+        cards.append(shuffled_cards[card_index])
+        card_index = card_index + 4
+      end
+      result.append({'player':player, 'cards': cards})
+    end
+    puts result
+    return result
+  end
+
 end
