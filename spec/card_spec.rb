0841af3661dfda8a0bfc74f742c1e4d57c5f9076
Md Farukuzzaman <farukuzzaman.bd@gmail.com>
2024-03-23 16:41:04 -0500

    Added test code for card suit method.

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -9,4 +9,13 @@ describe Card do
       expect(card.make_values).to eql(result)
     end
   end
+
+  describe "#suits" do
+    it "make spade suit" do
+      card = Card.new
+      values = ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K','A']
+      result = {'suit': 'spade', 'values': values}
+      expect(card.make_suits).to eql(result)
+    end
+  end
 end
