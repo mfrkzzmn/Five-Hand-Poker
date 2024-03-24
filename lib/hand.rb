42854b8a42b1391c3798a07fa76882d299090c5a
Md Farukuzzaman <farukuzzaman.bd@gmail.com>
2024-03-23 22:14:46 -0500

    refactoring high card codes and test codes.

--- lib/hand.rb
+++ lib/hand.rb
@@ -4,7 +4,7 @@ class Hand
     @cards = cards
   end
 
-  def check_high_hand(hand1, hand2)
+  def get_hand_index_array(hand1, hand2)
     values = ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K','A']
     hand1_index = []
     hand2_index = []
@@ -21,6 +21,11 @@ class Hand
     end
     hand2_index.sort!{|a,b| b<=>a}
 
+    return hand1_index, hand2_index
+  end
+
+  def check_high_hand(hand1, hand2)
+    hand1_index, hand2_index = get_hand_index_array(hand1, hand2)
     result = hand1_index <=> hand2_index
     return result
   end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -14,6 +14,36 @@ describe Hand do
     end
   end
 
+  describe "#get_hand_card_index" do
+    it "both hands card index array" do
+      hand = Hand.new(@cards)
+      hand1 = []
+      hand2 = []
+      suits = ["spade", "hearts", "club", "diamond"]
+      values1 = ['A', 'J', '8', '6', '7']
+      values2 = ['A', 'J', '8', '6', '7']
+      values = ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K','A']
+
+      hand1.append({'suit':suits[1], 'value': values1[0]})
+      hand1.append({'suit':suits[0], 'value': values1[1]})
+      hand1.append({'suit':suits[2], 'value': values1[2]})
+      hand1.append({'suit':suits[0], 'value': values1[3]})
+      hand1.append({'suit':suits[3], 'value': values1[4]})
+
+      hand2.append({'suit':suits[0], 'value': values2[0]})
+      hand2.append({'suit':suits[1], 'value': values2[1]})
+      hand2.append({'suit':suits[0], 'value': values2[2]})
+      hand2.append({'suit':suits[3], 'value': values2[3]})
+      hand2.append({'suit':suits[2], 'value': values2[4]})
+
+      hand1_index = [12, 9, 6, 5, 4]
+      hand2_index = [12, 9, 6, 5, 4]
+
+      a, b = hand.get_hand_index_array(hand1, hand2)
+      expect(hand.get_hand_index_array(hand1, hand2)).to match_array([hand1_index, hand2_index])
+    end
+  end
+
   describe "#high_card" do
     it "first hand is greater" do
       hand = Hand.new(@cards)
@@ -67,7 +97,7 @@ describe Hand do
       hand2 = []
       suits = ["spade", "hearts", "club", "diamond"]
       values1 = ['A', 'J', '8', '6', '7']
-      values2 = ['A', 'J', '8', '6', '7']
+      values2 = ['A', 'K', '4', '3', '2']
 
       hand1.append({'suit':suits[1], 'value': values1[0]})
       hand1.append({'suit':suits[0], 'value': values1[1]})
@@ -81,9 +111,7 @@ describe Hand do
       hand2.append({'suit':suits[3], 'value': values2[3]})
       hand2.append({'suit':suits[2], 'value': values2[4]})
 
-      expect(hand.check_high_hand(hand1, hand2)).to eql(0)
+      expect(hand.check_high_hand(hand1, hand2)).to eql(-1)
     end
-
   end
-
 end
