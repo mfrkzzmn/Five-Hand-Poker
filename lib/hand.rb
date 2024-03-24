9508ec3dac43c4425b1c48d0667dd1d8f8d3a79d
Md Farukuzzaman <farukuzzaman.bd@gmail.com>
2024-03-23 22:59:06 -0500

    refactoring high card codes and test codes.

--- lib/hand.rb
+++ lib/hand.rb
@@ -4,28 +4,22 @@ class Hand
     @cards = cards
   end
 
-  def get_hand_index_array(hand1, hand2)
+  def get_hand_index_array(hand)
     values = ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K','A']
-    hand1_index = []
-    hand2_index = []
+    hand_index = []
 
-    hand1.each do |hand|
-      index = values.find_index(hand[:value])
-      hand1_index.append(index)
+    hand.each do |h|
+      index = values.find_index(h[:value])
+      hand_index.append(index)
     end
-    hand1_index.sort!{|a,b| b<=>a}
+    hand_index.sort!{|a,b| b<=>a}
 
-    hand2.each do |hand|
-      index = values.find_index(hand[:value])
-      hand2_index.append(index)
-    end
-    hand2_index.sort!{|a,b| b<=>a}
-
-    return hand1_index, hand2_index
+    return hand_index
   end
 
   def check_high_hand(hand1, hand2)
-    hand1_index, hand2_index = get_hand_index_array(hand1, hand2)
+    hand1_index = get_hand_index_array(hand1)
+    hand2_index = get_hand_index_array(hand2)
     result = hand1_index <=> hand2_index
     return result
   end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -15,14 +15,14 @@ describe Hand do
   end
 
   describe "#get_hand_card_index" do
-    it "both hands card index array" do
+    it "get hand card index array" do
       hand = Hand.new(@cards)
+
+      values = ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K','A']
       hand1 = []
-      hand2 = []
       suits = ["spade", "hearts", "club", "diamond"]
       values1 = ['A', 'J', '8', '6', '7']
-      values2 = ['A', 'J', '8', '6', '7']
-      values = ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K','A']
+
 
       hand1.append({'suit':suits[1], 'value': values1[0]})
       hand1.append({'suit':suits[0], 'value': values1[1]})
@@ -30,17 +30,9 @@ describe Hand do
       hand1.append({'suit':suits[0], 'value': values1[3]})
       hand1.append({'suit':suits[3], 'value': values1[4]})
 
-      hand2.append({'suit':suits[0], 'value': values2[0]})
-      hand2.append({'suit':suits[1], 'value': values2[1]})
-      hand2.append({'suit':suits[0], 'value': values2[2]})
-      hand2.append({'suit':suits[3], 'value': values2[3]})
-      hand2.append({'suit':suits[2], 'value': values2[4]})
-
       hand1_index = [12, 9, 6, 5, 4]
-      hand2_index = [12, 9, 6, 5, 4]
 
-      a, b = hand.get_hand_index_array(hand1, hand2)
-      expect(hand.get_hand_index_array(hand1, hand2)).to match_array([hand1_index, hand2_index])
+      expect(hand.get_hand_index_array(hand1)).to match_array(hand1_index)
     end
   end
 
