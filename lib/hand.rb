8a25430067536d6ce010bf957b7d1badeb8b7271
Md Farukuzzaman <farukuzzaman.bd@gmail.com>
2024-03-23 21:46:21 -0500

    implemented code and tests for high cards logic.

--- lib/hand.rb
+++ lib/hand.rb
@@ -4,5 +4,25 @@ class Hand
     @cards = cards
   end
 
+  def check_high_hand(hand1, hand2)
+    values = ['2', '3', '4', '5', '6', '7', '8', '9','10', 'J', 'Q', 'K','A']
+    hand1_index = []
+    hand2_index = []
+
+    hand1.each do |hand|
+      index = values.find_index(hand[:value])
+      hand1_index.append(index)
+    end
+    hand1_index.sort!{|a,b| b<=>a}
+
+    hand2.each do |hand|
+      index = values.find_index(hand[:value])
+      hand2_index.append(index)
+    end
+    hand2_index.sort!{|a,b| b<=>a}
+
+    result = hand1_index <=> hand2_index
+    return result
+  end
 
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -15,7 +15,30 @@ describe Hand do
   end
 
   describe "#high_card" do
-    it "high cards in hand" do
+    it "first hand is greater" do
+      hand = Hand.new(@cards)
+      hand1 = []
+      hand2 = []
+      suits = ["spade", "hearts", "club", "diamond"]
+      values1 = ['A', 'J', '9', '6', '10']
+      values2 = ['A', 'J', '4', '7', '8']
+
+      hand1.append({'suit':suits[1], 'value': values1[0]})
+      hand1.append({'suit':suits[0], 'value': values1[1]})
+      hand1.append({'suit':suits[2], 'value': values1[2]})
+      hand1.append({'suit':suits[0], 'value': values1[3]})
+      hand1.append({'suit':suits[3], 'value': values1[4]})
+
+      hand2.append({'suit':suits[0], 'value': values2[0]})
+      hand2.append({'suit':suits[1], 'value': values2[1]})
+      hand2.append({'suit':suits[0], 'value': values2[2]})
+      hand2.append({'suit':suits[3], 'value': values2[3]})
+      hand2.append({'suit':suits[2], 'value': values2[4]})
+
+      expect(hand.check_high_hand(hand1, hand2)).to eql(1)
+    end
+
+    it "second hand is greater" do
       hand = Hand.new(@cards)
       hand1 = []
       hand2 = []
@@ -35,8 +58,32 @@ describe Hand do
       hand2.append({'suit':suits[3], 'value': values2[3]})
       hand2.append({'suit':suits[2], 'value': values2[4]})
 
-      expect(deck.check_high_hand(hand1, hand2)).to eql(false)
+      expect(hand.check_high_hand(hand1, hand2)).to eql(-1)
     end
+
+    it "both hands are equal" do
+      hand = Hand.new(@cards)
+      hand1 = []
+      hand2 = []
+      suits = ["spade", "hearts", "club", "diamond"]
+      values1 = ['A', 'J', '8', '6', '7']
+      values2 = ['A', 'J', '8', '6', '7']
+
+      hand1.append({'suit':suits[1], 'value': values1[0]})
+      hand1.append({'suit':suits[0], 'value': values1[1]})
+      hand1.append({'suit':suits[2], 'value': values1[2]})
+      hand1.append({'suit':suits[0], 'value': values1[3]})
+      hand1.append({'suit':suits[3], 'value': values1[4]})
+
+      hand2.append({'suit':suits[0], 'value': values2[0]})
+      hand2.append({'suit':suits[1], 'value': values2[1]})
+      hand2.append({'suit':suits[0], 'value': values2[2]})
+      hand2.append({'suit':suits[3], 'value': values2[3]})
+      hand2.append({'suit':suits[2], 'value': values2[4]})
+
+      expect(hand.check_high_hand(hand1, hand2)).to eql(0)
+    end
+
   end
 
 end
